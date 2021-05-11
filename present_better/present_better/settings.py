b2b6ae0c3e2f866dfa85e71e25d1424991383b7d
Min Kyu Han <minkyu.han1029@gmail.com>
2021-05-11 16:42:10 -0400

    Fixed bug

--- present_better/present_better/settings.py
+++ present_better/present_better/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pbApp.apps.PbappConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
